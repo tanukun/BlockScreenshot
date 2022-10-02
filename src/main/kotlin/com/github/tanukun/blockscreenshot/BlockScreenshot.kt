4c66a5452458941de52cd6b43e478028fa2ca22a
tanukun <taanukunn@gmail.com>
2022-10-02 23:38:09 +0900

    連続、単発モード

--- src/main/kotlin/com/github/tanukun/blockscreenshot/BlockScreenshot.kt
+++ src/main/kotlin/com/github/tanukun/blockscreenshot/BlockScreenshot.kt
@@ -1,6 +1,5 @@
 package com.github.tanukun.blockscreenshot
 
-import com.google.common.util.concurrent.Futures
 import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
@@ -10,6 +9,7 @@ import net.minecraft.command.argument.EntityAnchorArgumentType
 import net.minecraft.command.argument.Vec3ArgumentType
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
@@ -17,8 +17,6 @@ import net.minecraft.util.registry.Registry
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 import java.util.*
-import java.util.concurrent.Future
-import java.util.concurrent.FutureTask
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlin.random.Random
@@ -34,76 +32,106 @@ object BlockScreenshot : ModInitializer {
     /** 遅延実行用 */
     private val timer = Timer()
 
+    /** プレイヤーごとのタスク */
+    private val playerTasks = mutableMapOf<ServerPlayerEntity, TimerTask>()
+
     override fun onInitialize() {
         CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _, _ ->
+            // 単発スクショ
             dispatcher.register(
                 CommandManager.literal("screenshot")
                     .then(
                         CommandManager.argument("pos", Vec3ArgumentType.vec3(true))
                             .executes { context: CommandContext<ServerCommandSource> ->
-                                replaceAndTakeScreenshot(context)
+                                // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+                                //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
+                                context.source.player?.let { player ->
+                                    val center = Vec3ArgumentType.getVec3(context, "pos")
+                                    replaceAndTakeScreenshot(player, center)
+                                }
                                 1
                             })
             )
 
+            // 連続スクショ
             dispatcher.register(
-                CommandManager.literal("screenshot_random_location")
-                    .executes { context: CommandContext<ServerCommandSource> ->
-                        teleportReplaceAndTakeScreenshot(context)
-                        1
-                    })
+                CommandManager.literal("screenshot_loop")
+                    .then(
+                        CommandManager.argument("pos", Vec3ArgumentType.vec3(true))
+                            .executes { context: CommandContext<ServerCommandSource> ->
+                                // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+                                //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
+                                context.source.player?.let { player ->
+                                    val taskToStop = playerTasks.remove(player)
+                                    if (taskToStop != null) {
+                                        // タスクがあればキャンセル
+                                        taskToStop.cancel()
+                                    } else {
+                                        // タスクがなければ実行する
+                                        // 中心座標
+                                        val center = Vec3ArgumentType.getVec3(context, "pos")
+                                        // タスクを作成する
+                                        val task = object : TimerTask() {
+                                            override fun run() {
+                                                replaceAndTakeScreenshot(player, center)
+                                            }
+                                        }
+                                        // タスクを登録する
+                                        timer.scheduleAtFixedRate(task, 0, 100)
+                                        // タスクを保存する
+                                        playerTasks[player] = task
+                                    }
+                                }
+                                1
+                            })
+            )
         })
     }
 
-    private fun replaceAndTakeScreenshot(context: CommandContext<ServerCommandSource>) {
+    private fun replaceAndTakeScreenshot(player: ServerPlayerEntity, center: Vec3d) {
         // ブロックを置く座標
-        val center = Vec3ArgumentType.getVec3(context, "pos")
         val centerBlockPos = BlockPos(center)
         val centerVec =
             Vec3d(centerBlockPos.x + 0.5, centerBlockPos.y + 0.5, centerBlockPos.z + 0.5)
 
-        // For versions below 1.19, replace "Text.literal" with "new LiteralText".
-        //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
-        context.source.player?.let { player ->
-            // ワールド
-            val world = player.getWorld()
-            // ランダムにブロックを選択
-            val randomBlock = Registry.BLOCK[Random.nextInt(Registry.BLOCK.size())]
-            // ランダムなブロックを設置
-            world.setBlockState(centerBlockPos, randomBlock.defaultState)
-            // ランダムな向き
-            val yaw = Random.nextDouble() * 360.0
-            val pitch = (Random.nextDouble() - 0.5f) * 180.0
-            // 今の座標
-            val xz = cos(Math.toRadians(pitch))
-            val vec = Vec3d(
-                -xz * sin(Math.toRadians(yaw)),
-                -sin(Math.toRadians(pitch)),
-                xz * cos(Math.toRadians(yaw))
-            )
-            // プレイヤーをテレポート
-            player.teleport(
-                world,
-                centerVec.x + vec.x * 3,
-                centerVec.y + vec.y * 3,
-                centerVec.z + vec.z * 3,
-                yaw.toFloat(),
-                pitch.toFloat()
-            )
-            // 中心を見る
-            player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, centerVec)
-            // パケット作成
-            val buf = PacketByteBufs.create()
-            buf.writeText(randomBlock.name)
+        // ワールド
+        val world = player.getWorld()
+        // ランダムにブロックを選択
+        val randomBlock = Registry.BLOCK[Random.nextInt(Registry.BLOCK.size())]
+        // ランダムなブロックを設置
+        world.setBlockState(centerBlockPos, randomBlock.defaultState)
+        // ランダムな向き
+        val yaw = Random.nextDouble() * 360.0
+        val pitch = (Random.nextDouble() - 0.5f) * 180.0
+        // 今の座標
+        val xz = cos(Math.toRadians(pitch))
+        val vec = Vec3d(
+            -xz * sin(Math.toRadians(yaw)),
+            -sin(Math.toRadians(pitch)),
+            xz * cos(Math.toRadians(yaw))
+        )
+        // プレイヤーをテレポート
+        player.teleport(
+            world,
+            centerVec.x + vec.x * 1.5,
+            centerVec.y + vec.y * 1.5,
+            centerVec.z + vec.z * 1.5,
+            yaw.toFloat(),
+            pitch.toFloat()
+        )
+        // 中心を見る
+        player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, centerVec)
+        // パケット作成
+        val buf = PacketByteBufs.create()
+        buf.writeText(randomBlock.name)
 
-            // ちょっと後に実行
-            timer.schedule(object: TimerTask() {
-                override fun run() {
-                    // スクショを撮る
-                    ServerPlayNetworking.send(player, SCREENSHOT_PACKET_ID, buf)
-                }
-            }, 200)
-        }
+        // ちょっと後に実行
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                // スクショを撮る
+                ServerPlayNetworking.send(player, SCREENSHOT_PACKET_ID, buf)
+            }
+        }, 50)
     }
 
     private fun teleportReplaceAndTakeScreenshot(context: CommandContext<ServerCommandSource>) {
