0e549427e795abee9816d65257a460927b7680d9
tanukun <taanukunn@gmail.com>
2022-10-02 22:51:38 +0900

    スクショを撮る

--- src/main/kotlin/com/github/tanukun/blockscreenshot/BlockScreenshot.kt
+++ src/main/kotlin/com/github/tanukun/blockscreenshot/BlockScreenshot.kt
@@ -1,7 +1,137 @@
 package com.github.tanukun.blockscreenshot
 
+import com.google.common.util.concurrent.Futures
+import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
+import net.minecraft.command.argument.EntityAnchorArgumentType
+import net.minecraft.command.argument.Vec3ArgumentType
+import net.minecraft.server.command.CommandManager
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.util.Identifier
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Vec3d
+import net.minecraft.util.registry.Registry
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.util.*
+import java.util.concurrent.Future
+import java.util.concurrent.FutureTask
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.random.Random
+import kotlin.random.nextInt
 
 object BlockScreenshot : ModInitializer {
-    override fun onInitialize() {}
+    /** ロガー */
+    val LOGGER: Logger = LogManager.getLogger("BlockScreenshot")
+
+    /** スクリーンショットのパケットID */
+    val SCREENSHOT_PACKET_ID = Identifier.of("blockscreenshot", "take_screenshot")
+
+    /** 遅延実行用 */
+    private val timer = Timer()
+
+    override fun onInitialize() {
+        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher, _, _ ->
+            dispatcher.register(
+                CommandManager.literal("screenshot")
+                    .then(
+                        CommandManager.argument("pos", Vec3ArgumentType.vec3(true))
+                            .executes { context: CommandContext<ServerCommandSource> ->
+                                replaceAndTakeScreenshot(context)
+                                1
+                            })
+            )
+
+            dispatcher.register(
+                CommandManager.literal("screenshot_random_location")
+                    .executes { context: CommandContext<ServerCommandSource> ->
+                        teleportReplaceAndTakeScreenshot(context)
+                        1
+                    })
+        })
+    }
+
+    private fun replaceAndTakeScreenshot(context: CommandContext<ServerCommandSource>) {
+        // ブロックを置く座標
+        val center = Vec3ArgumentType.getVec3(context, "pos")
+        val centerBlockPos = BlockPos(center)
+        val centerVec =
+            Vec3d(centerBlockPos.x + 0.5, centerBlockPos.y + 0.5, centerBlockPos.z + 0.5)
+
+        // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+        //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
+        context.source.player?.let { player ->
+            // ワールド
+            val world = player.getWorld()
+            // ランダムにブロックを選択
+            val randomBlock = Registry.BLOCK[Random.nextInt(Registry.BLOCK.size())]
+            // ランダムなブロックを設置
+            world.setBlockState(centerBlockPos, randomBlock.defaultState)
+            // ランダムな向き
+            val yaw = Random.nextDouble() * 360.0
+            val pitch = (Random.nextDouble() - 0.5f) * 180.0
+            // 今の座標
+            val xz = cos(Math.toRadians(pitch))
+            val vec = Vec3d(
+                -xz * sin(Math.toRadians(yaw)),
+                -sin(Math.toRadians(pitch)),
+                xz * cos(Math.toRadians(yaw))
+            )
+            // プレイヤーをテレポート
+            player.teleport(
+                world,
+                centerVec.x + vec.x * 3,
+                centerVec.y + vec.y * 3,
+                centerVec.z + vec.z * 3,
+                yaw.toFloat(),
+                pitch.toFloat()
+            )
+            // 中心を見る
+            player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, centerVec)
+            // パケット作成
+            val buf = PacketByteBufs.create()
+            buf.writeText(randomBlock.name)
+
+            // ちょっと後に実行
+            timer.schedule(object: TimerTask() {
+                override fun run() {
+                    // スクショを撮る
+                    ServerPlayNetworking.send(player, SCREENSHOT_PACKET_ID, buf)
+                }
+            }, 200)
+        }
+    }
+
+    private fun teleportReplaceAndTakeScreenshot(context: CommandContext<ServerCommandSource>) {
+        // For versions below 1.19, replace "Text.literal" with "new LiteralText".
+        //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
+        context.source.player?.let { player ->
+            // ワールド
+            val world = player.getWorld()
+            // ランダムな座標
+            val randomX = player.blockX + Random.nextInt(-100..100)
+            val randomZ = player.blockZ + Random.nextInt(-100..100)
+            // XZ座標の地表を探す
+            val randomPos = (world.topY downTo world.bottomY)
+                .map { BlockPos(randomX, it, randomZ) }
+                .first { !world.isAir(it) }
+            // ランダムにブロックを選択
+            val randomBlock = Registry.BLOCK[Random.nextInt(Registry.BLOCK.size())]
+            // ランダムなブロックを設置
+            world.setBlockState(randomPos, randomBlock.defaultState)
+            // プレイヤーをテレポート
+            player.teleport(
+                world,
+                randomPos.x + 0.5,
+                randomPos.y + 0.5,
+                randomPos.z + 0.5,
+                player.yaw,
+                player.pitch
+            )
+        }
+    }
 }
--- src/main/kotlin/com/github/tanukun/blockscreenshot/client/BlockScreenshotClient.kt
+++ src/main/kotlin/com/github/tanukun/blockscreenshot/client/BlockScreenshotClient.kt
@@ -1,28 +1,101 @@
 package com.github.tanukun.blockscreenshot.client
 
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.context.CommandContext
+import com.github.tanukun.blockscreenshot.BlockScreenshot
+import com.github.tanukun.blockscreenshot.BlockScreenshot.SCREENSHOT_PACKET_ID
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
-import net.minecraft.command.CommandRegistryAccess
-import net.minecraft.server.command.CommandManager
-import net.minecraft.server.command.CommandManager.RegistrationEnvironment
-import net.minecraft.server.command.ServerCommandSource
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gl.Framebuffer
+import net.minecraft.client.gl.SimpleFramebuffer
+import net.minecraft.client.util.ScreenshotRecorder
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.MutableText
+import net.minecraft.text.Style
+import net.minecraft.text.Text
+import net.minecraft.util.Formatting
+import java.io.File
 
 @Environment(EnvType.CLIENT)
 object BlockScreenshotClient : ClientModInitializer {
     override fun onInitializeClient() {
-        CommandRegistrationCallback.EVENT.register(CommandRegistrationCallback { dispatcher: CommandDispatcher<ServerCommandSource?>, registryAccess: CommandRegistryAccess?, environment: RegistrationEnvironment? ->
-            dispatcher.register(CommandManager.literal("screenshot")
-                .executes { context: CommandContext<ServerCommandSource> ->
-                    // For versions below 1.19, replace "Text.literal" with "new LiteralText".
-                    //context.getSource().sendMessage(Text.literal("Called /foo with no arguments"));
-                    val player = context.source.player
-                    player!!.teleport(player.getWorld(), player.x, player.y + 1, player.z, player.yaw, player.pitch)
-                    1
-                })
-        })
+        // スクショ保存フォルダ
+        val minecraft = MinecraftClient.getInstance()
+        val screenshotDir = File(minecraft.runDirectory, "block_screenshot")
+
+        // パケットが来たらスクショを作成
+        ClientPlayNetworking.registerGlobalReceiver(SCREENSHOT_PACKET_ID) { client, _, buf, _ ->
+            // ブロックの名前
+            val blockName = buf.readText()
+            // フォルダを作成
+            val blockDir = File(screenshotDir, blockName.string)
+            blockDir.mkdirs()
+
+            // execute内ではレンダリングスレッドで実行される
+            client.execute {
+                // スクリーンショットを取る
+                val resultText = client.takeScreenshot(blockDir, 512, 512)
+                // チャットに表示
+                client.inGameHud.chatHud.addMessage(resultText)
+            }
+        }
+    }
+
+    private fun MinecraftClient.takeScreenshot(directory: File, width: Int, height: Int): Text? {
+        val window = this.window
+        val player = this.player
+            ?: return null
+        val framebufferWidth: Int = window.framebufferWidth
+        val framebufferHeight: Int = window.framebufferHeight
+        val framebuffer: Framebuffer = SimpleFramebuffer(width, height, true, MinecraftClient.IS_SYSTEM_MAC)
+        val pitch: Float = player.pitch
+        val yaw: Float = player.yaw
+        val prevPitch: Float = player.prevPitch
+        val prevYaw: Float = player.prevYaw
+        this.gameRenderer.setBlockOutlineEnabled(false)
+        var resultText: MutableText?
+        try {
+            this.gameRenderer.isRenderingPanorama = true
+            this.worldRenderer.reloadTransparencyShader()
+            window.framebufferWidth = width
+            window.framebufferHeight = height
+
+            player.prevYaw = player.yaw
+            player.prevPitch = player.pitch
+            framebuffer.beginWrite(true)
+            this.gameRenderer.renderWorld(1.0f, 0L, MatrixStack())
+            try {
+                Thread.sleep(10L)
+            } catch (_: InterruptedException) {
+            }
+            ScreenshotRecorder.saveScreenshot(directory, framebuffer) {}
+
+            val text: Text = Text.literal(directory.name).formatted(Formatting.UNDERLINE).styled { style: Style ->
+                style.withClickEvent(
+                    ClickEvent(ClickEvent.Action.OPEN_FILE, directory.absolutePath)
+                )
+            }
+            resultText = Text.translatable("screenshot.success", *arrayOf<Any>(text))
+            return resultText
+        } catch (e: Exception) {
+            BlockScreenshot.LOGGER.error("Couldn't save image", e)
+            resultText = Text.translatable("screenshot.failure", *arrayOf<Any?>(e.message))
+        } finally {
+            player.pitch = pitch
+            player.yaw = yaw
+            player.prevPitch = prevPitch
+            player.prevYaw = prevYaw
+            this.gameRenderer.setBlockOutlineEnabled(true)
+            window.framebufferWidth = framebufferWidth
+            window.framebufferHeight = framebufferHeight
+            framebuffer.delete()
+            this.gameRenderer.isRenderingPanorama = false
+            this.worldRenderer.reloadTransparencyShader()
+            this.framebuffer.beginWrite(true)
+        }
+
+        return resultText
     }
 }
